40f96db67c0e3578a213275e992a3921a5d1e4ac
reyno405 <reyno405@gmail.com>
2022-09-10 15:20:10 -0400

    this has the function to move hands on clock according to time of day

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ function getTime() {
     const hours = now.getHours()
     const timeInterval = 6
 
-    secondsHand.style.transform = 'rotate(60deg)'
+    
+
+    secondsHand.style.transform = 'rotate(' + (seconds * timeInterval) + 'deg )'
+    minutesHand.style.transform = 'rotate(' + (minutes * timeInterval + seconds / 10) + 'deg )'
+    hoursHand.style.transform = 'rotate(' + (hours * 30 + minutes / 2 ) + 'deg )'
 
 }
 
-getTime()
+setInterval(getTime, 100)
